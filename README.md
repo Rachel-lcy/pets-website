58ceeb4b4f6da536c0a78e7e09568f8cacaf8584
Rachel <hurui.lcy@gmail.com>
2025-03-02 17:23:58 -0700

    dashboard finished and create README

--- README.md
+++ README.md
@@ -26,16 +26,25 @@ create .env.local file
 Add ClerkProvider to the layout file
 ```
 
-## Deploy on Vercel
+## Design Decision
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+````Visual Layout:
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+A clean and modern design was chosen to ensure an intuitive user experience.
 
-```
+A masonry grid layout was implemented for the pet image gallery to create a visually appealing and organized display.
 
-```
+The dashboard page was designed with card components to categorize pet information effectively.
 
-```
+```Typography and Color Scheme:
 
-```
+The application uses a simple, readable font style to enhance accessibility.
+
+Soft pastel and neutral colors were used to create a welcoming and warm atmosphere.
+
+```Responsive Design:
+
+The website is fully responsive and optimized for different screen sizes, including mobile, tablet, and desktop views.
+
+A flexible grid system ensures elements resize and rearrange dynamically based on the viewport.
+````
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,27 @@
 "use client"
 import React from 'react';
 import { useUser } from '@clerk/nextjs';
-import Icon from "./icon.svg";
+import Dog from "./dog.png";
+import Contact from "./contact.png";
+import Owner from "./petowner.png";
+import Sitter from "./sitting.png";
+import Grooming from "./grooming.png";
+import Pet from "./pet.png";
+import Veterinarian from "./veterinarian.png";
+import PersianCat from "./persian-cat.png";
+import Corgi from "./corgi.png";
+import Labrador from "./labrador-retriever.png";
+import ScottishFold from "./scottish-fold.png";
+import Poddle from "./Poodle.png";
+import SiameseCat from "./siamese-cat.png";
+
+
+
+import Activity from "./activity.jpg"
+import Toys from "./toys.jpg"
+import Behavior from "./behavior.jpg"
+import Traning from "./traning.jpg"
+
 import Image from 'next/image';
 
 const DashboardPage = () => {
@@ -9,26 +29,197 @@ const DashboardPage = () => {
   if(!isSignedIn) return <h1 className="text-2xl font-bold"> Loading...</h1>
 
   return (
-    <div className="p-4 text-xl font-bold">
-        <h1 className="text-2xl font-bold">
-        Welcome, {user?.firstName || "Guest"};<br />
+    <main className='box-border max-w-full justify-center justify-items-center' >
+
+      <div className="p-4 font-bold my-5">
+        <h1 className="text-3xl font-bold tracking-wide mb-4">
+        Welcome, {user?.firstName || "Guest"}!<br />
+        This is a Lovely place for pets lover!
         </h1>
-        <small>
-          This is a Lovely place for pets lover!
-        </small>
-        <div>
-          <div className='flex'>
+      </div>
+
+      <div>
+        <div className='flex'>
+              <Image
+                src={Pet.src}
+                alt='Icon'
+                width = {50}
+                height={50}
+                quality ={100}
+              />
+              <h3>PET OVERVIEW</h3>
+        </div>
+
+        <div className='shadow-md '>
+          <div className="card">
+            <Image className='card-image' src={PersianCat.src}
+            width ={440}
+            height = {327}
+            alt="lovely animals"
+            />
+            <h2 className='card-title '>British Shorthair Cat</h2>
+            <p className='card-text '>2 yeats old</p>
+          </div>
+
+          <div className="card flex">
+            <Image className='card-image' src={Corgi.src}
+            width ={440}
+            height = {327}
+            alt="lovely animals"
+            />
+            <h2 className='card-title'>Corgi</h2>
+            <p className='card-text'>2 yeats old</p>
+          </div>
+
+          <div className="card">
+            <Image className='card-image' src={Labrador.src}
+            width ={440}
+            height = {327}
+            alt="lovely animals"
+            />
+            <h2 className='card-title'>Labrador</h2>
+            <p className='card-text'>2 yeats old</p>
+          </div>
+
+          <div className="card">
+            <Image className='card-image' src={ScottishFold.src}
+            width ={440}
+            height = {327}
+            alt="lovely animals"
+            />
+            <h2 className='card-title'>Scottish Fold Cat</h2>
+            <p className='card-text'>2 yeats old</p>
+          </div>
+
+          <div className="card">
+            <Image className='card-image' src={Poddle.src}
+            width ={440}
+            height = {327}
+            alt="lovely animals"
+            />
+            <h2 className='card-title'>Poddle</h2>
+            <p className='card-text'>2 yeats old</p>
+          </div>
+
+          <div className="card">
+            <Image className='card-image' src={SiameseCat.src}
+            width ={440}
+            height = {327}
+            alt="lovely animals"
+            />
+            <h2 className='card-title'>Siamese Cat</h2>
+            <p className='card-text'>2 yeats old</p>
+          </div>
+
+          <div className='flex my-5'>
             <Image
-              src={Icon.src}
+              src={Dog.src}
               alt='Icon'
               width = {50}
               height={50}
               quality ={100}
             />
-            <h3>PET OVERVIEW</h3>
+            <h3>PET FAVORITE THINGS & ACTIVITIES</h3>
+          </div>
+
+          <div className="card">
+            <Image className='card-image' 
+              src={Activity.src}
+              width ={440}
+              height = {327}
+              alt="lovely animals"
+            />
+            <h2 className='card-title '>Activity</h2>
+          </div>
+
+          <div className="card">
+            <Image className='card-image'
+              src={Toys.src}
+              width ={440}
+              height = {327}
+              alt="lovely animals"
+            />
+            <h2 className='card-title '>Toys</h2>
+          </div>
+
+          <div className="card">
+            <Image className='card-image'
+              src={Behavior.src}
+              width ={440}
+              height = {327}
+              alt="lovely animals"
+            />
+            <h2 className='card-title '>Behavior and Mood Notes</h2>
+          </div>
+
+          <div className="card">
+            <Image className='card-image' 
+              src={Traning.src}
+              width ={440}
+              height = {327}
+              alt="lovely animals"
+            />
+            <h2 className='card-title '>Training Commands</h2>
+          </div>
+
+          <div className='flex my-5'>
+            <Image
+              src={Contact.src}
+              alt='Icon'
+              width = {50}
+              height={50}
+              quality ={100}
+            />
+            <h3>CONTACT INFORMATION</h3>
+          </div>
+
+          <div className="card">
+            <Image className='card-image'
+              src={Owner.src}
+              width ={440}
+              height = {327}
+              alt="lovely animals"
+            />
+            <h2 className='card-title '>Pets Owner</h2>
+            <p>John Don</p>
+          </div>
+
+          <div className="card">
+            <Image className='card-image'
+              src={Grooming.src}
+              width ={440}
+              height = {327}
+              alt="lovely animals"
+            />
+            <h2 className='card-title '>Pet Grooming</h2>
+            <p>Honey's Pet Grooming</p>
+          </div>
+
+          <div className="card">
+            <Image className='card-image'
+              src={Veterinarian.src}
+              width ={440}
+              height = {327}
+              alt="lovely animals"
+            />
+            <h2 className='card-title '>Veterinarian</h2>
+            <p>Ana West</p>
+          </div>
+
+          <div className="card">
+            <Image className='card-image'
+              src={Sitter.src}
+              width ={440}
+              height = {327}
+              alt="lovely animals"
+            />
+            <h2 className='card-title '>Pet Sitter</h2>
+            <p>Dogs Walkers and Pet Sitters</p>
           </div>
         </div>
-    </div>
+      </div>
+    </main>
+    
   )
 }
 export default DashboardPage
